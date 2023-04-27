59b696ea88d916b0d1c80785cc387232ea595ccc
Alekss <aleks.kalendra12345@gmail.com>
2023-04-27 04:47:06 -0700

    next commit

--- DressUpGame/Assets/skripti/SakumsScript.cs
+++ DressUpGame/Assets/skripti/SakumsScript.cs
@@ -6,34 +6,11 @@ using System;
 using UnityEngine.SceneManagement;
 
 public class SakumsScript : MonoBehaviour {
-	public GameObject Play;
-	public GameObject Exit;
-	public GameObject dropdown;
-	public GameObject Rick;
-	public GameObject Morty;
-
-
-	public void playBtn(){
-		mortyBtn ();
-	}
-	public void mainaTelu (int indekss){
-		if (indekss == 0) {
-			Rick.SetActive (false);
-			Morty.SetActive (true);
-		}
-		if (indekss == 1) {
-			Rick.SetActive (true);
-			Morty.SetActive (false);
-		}
-	}
 	public void exitBtn(){
 		Application.Quit();
 	}
 	public void mortyBtn(){
 		SceneManager.LoadScene("MortyScene", LoadSceneMode.Single);
-	}
-	public void rickBtn(){
-		SceneManager.LoadScene("RickScene", LoadSceneMode.Single);
 	}
 	public void menu(){
 		SceneManager.LoadScene("Sakums", LoadSceneMode.Single);
---
+++ DressUpGame/Assets/skripti/atteluMainisana.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class atteluMainisana : MonoBehaviour {
+    public GameObject changingImg;
+    public Sprite[] imgArr;
+    public GameObject sliderW;
+    public GameObject sliderH;
+
+    public void mainaTelu(int skaitlis)
+    {
+        if (skaitlis == 0)
+            changingImg.GetComponent<Image>().sprite = imgArr[0];
+        else
+            changingImg.GetComponent<Image>().sprite = imgArr[1];
+    }
+    public void mainaPlatumu()
+    {
+        float vertibaW = sliderW.GetComponent<Slider>().value;
+        changingImg.transform.localScale = new Vector3(1F, 1F*vertibaW,1);
+    }
+    public void mainaAugstumu()
+    {
+        float vertibaH = sliderH.GetComponent<Slider>().value;
+        changingImg.transform.localScale = new Vector3(1F * vertibaH, 1F , 1);
+    }
+
+}
